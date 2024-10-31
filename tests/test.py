8150e27eb7486418a42948f1be64481bbb4305ba
monstermichl <vouillarmet.michel@gmail.com>
2024-10-31 07:31:00 +0100

    Update test file

--- tests/test.py
+++ tests/test.py
@@ -2,12 +2,33 @@ from os import path
 import pathlib
 import unittest
 
-from ninja_bear import Orchestrator
+from ninja_bear import Orchestrator, Plugin
 from ninja_bear.base.generator_configuration import GeneratorConfiguration
 from src.ninja_bear_language_go.generator import Generator
 
 
-_NINJA_BEAR_REFERENCE_REGEX = r'Generated with ninja-bear v\d+\.\d+\.\d+'
+# Desired code outcome when using ninja-bear-language-go.
+_COMPARE_FILE_CONTENT = """
+package testpackage
+
+var TestConfig = struct {
+    myBoolean           bool
+    myInteger           int
+    myFloat             float64
+    myCombinedFloat     float64
+    myDouble            float64
+    myRegex             string // Just another RegEx.
+    mySubstitutedString string
+}{
+    myBoolean:           true,
+    myInteger:           142,
+    myFloat:             322.0,
+    myCombinedFloat:     45724.0,
+    myDouble:            233.9,
+    myRegex:             "Test Reg(E|e)x", // Just another RegEx.
+    mySubstitutedString: "Sometimes I just want to scream Hello World!",
+}
+"""
 
 
 class Test(unittest.TestCase):
@@ -15,16 +36,22 @@ class Test(unittest.TestCase):
         super().__init__(methodName)
         self._test_path = pathlib.Path(__file__).parent.resolve()
         self._test_config_path = path.join(self._test_path, '..', 'example/test-config.yaml')
-        self._test_compare_files_path = path.join(self._test_path, 'compare_files')
+        self._plugins = [
+            Plugin('config', Generator)
+        ]
 
     def test_run_generators(self):
-        orchestrator = Orchestrator.read_config(self._test_config_path)
+        orchestrator = Orchestrator.read_config(self._test_config_path, plugins=self._plugins)
         language_configs = orchestrator.language_configs
 
         self.assertEqual(len(language_configs), 1)
 
         language_config = language_configs[0]
         config_generator = language_config.generator
+
+        # This is required to use the local implementation of the generator. Otherwise, the
+        # plugin would be executed by the ninja-bear module and the code coverage couldn't
+        # be checked.
         local_generator = Generator(
             GeneratorConfiguration(
                 indent=config_generator._indent,
@@ -36,49 +63,5 @@ class Test(unittest.TestCase):
             additional_props=config_generator._additional_props,
         )
 
-        compare_file_path = path.join(
-            self._test_compare_files_path,
-            f'{language_config.config_info.file_name_full}'
-        )
-        
-        with open(compare_file_path, 'r') as f:
-            content 
-
-class Test(unittest.TestCase):
-    def __init__(self, methodName: str = "runTest") -> None:
-        super().__init__(methodName)
-        self._test_path = pathlib.Path(__file__).parent.resolve()
-        self._test_config_path = path.join(self._test_path, '..', 'example/test-config.yaml')
-        self._test_compare_files_path = path.join(self._test_path, 'compare_files')
-
-    def test_run_generators(self):
-        orchestrator = Orchestrator.read_config(self._test_config_path)
-        language_configs = orchestrator.language_configs
-
-        self.assertEqual(len(language_configs), 1)
-
-        language_config = language_configs[0]
-        config_generator = language_config.generator
-        local_generator = Generator(
-            GeneratorConfiguration(
-                indent=config_generator._indent,
-                transformers=config_generator.transformers,
-                naming_conventions=config_generator._naming_conventions,
-                type_name=config_generator._type_name
-            ),
-            properties=config_generator._properties,
-            additional_props=config_generator._additional_props,
-        )
-
-        compare_file_path = path.join(
-            self._test_compare_files_path,
-            f'{language_config.config_info.file_name_full}'
-        )
-        
-        with open(compare_file_path, 'r') as f:
-            content = f.read()
-
-        original_max_diff = self.maxDiff
         self.maxDiff = None
-        self.assertEqual(local_generator.dump(), content)
-        self.maxDiff = original_max_diff
+        self.assertEqual(local_generator.dump().strip(), _COMPARE_FILE_CONTENT.strip())
